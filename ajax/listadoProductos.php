87049f8ae6729902481661a2d30bb80cf55a5d68
Johan Montero <johan00712@gmail.com>
2026-03-03 00:57:55 -0500

    Actualizacion catalogo

--- ajax/listadoProductos.php
+++ ajax/listadoProductos.php
@@ -15,7 +15,7 @@
             from 
                 productos p
                 inner join presentacion_producto pp ON pp.id_producto  = p.id_producto
-                WHERE p.id_producto NOT IN (11,12)";
+                WHERE p.id_producto NOT IN (11,12, 15,16)";
 
                 
     $productos = $db->select($sql);
